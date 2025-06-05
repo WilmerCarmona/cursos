4ef9bf8e21e4af0ad08c530d9b5efbcd142c7f63
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-05 14:23:07 -0500

    update

---
+++ ULTIMATE_JS/02-operadores/01-aritmeticos.js
@@ -0,0 +1,21 @@
+// Se utilizan más para hacer operaciones matematicas
+
+let a = 5;
+let b = 4; 
+
+// console.log(a+b , 'Suma');
+// console.log(a-b , 'Resta');
+// console.log(a*b , 'Multiplicación');
+// console.log(a/b , 'Divicion');
+// console.log(a%b , 'Residuo');
+// console.log(a**b , 'Potencia');
+
+// Incrementar
+console.log(++a);
+console.log(a++);
+console.log(a);
+
+// Decrementar
+console.log(--a);
+console.log(a--);
+console.log(a);
