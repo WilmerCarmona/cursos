a51e5c7a0ae99262e44a386d98331468c58d80e7
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-06 13:02:34 -0500

    update

---
+++ ULTIMATE_JS/02-operadores/06-orden.js
@@ -0,0 +1,12 @@
+/* Esto es un ejemplo de orden 
+8/2 en JS aquí deberia de ir el signo de multiplicación (2+2)
+8/2(2+2)
+8/2(4)
+8/2 = 4 x 4
+4X4 = 16*/
+
+
+let resultado = 8/2*(2+2); /*Cada vez que se vea una operacion matematica que contenga estos parentesis
+ deberia de agregarse el signo de multiplicación antes */
+
+console.log(resultado);
