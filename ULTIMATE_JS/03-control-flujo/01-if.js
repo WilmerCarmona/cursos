3a7f1c70be7f54c1a420fa38abde52075543cb53
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-06 18:18:48 -0500

    update

---
+++ ULTIMATE_JS/03-control-flujo/01-if.js
@@ -0,0 +1,6 @@
+
+let edad = 25;
+
+if(edad > 17){
+    console.log('El usuario es mayor de edad');
+}
---
+++ ULTIMATE_JS/03-control-flujo/02-else.js
@@ -0,0 +1,21 @@
+// let edad = 19;
+
+// if(edad > 18){
+//     console.log('Es mayor de edad');
+// }else{
+//     console.log('Es menor de edad');
+// }
+
+// console.log(edad);
+
+// EJEMPLO CON IF ELSE
+
+const edad = 18;
+
+if(edad >= 18){
+    console.log('El usuario es mayor de edad');
+}else if(edad > 13){
+    console.log('El usuario es adolecente');
+}else{
+    console.log('El usuario es menor de edad');
+}
