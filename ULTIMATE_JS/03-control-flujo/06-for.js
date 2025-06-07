6a2ed8a5c789e71c331d06181d1c9c24a1a7539a
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-07 18:42:33 -0500

    update

---
+++ ULTIMATE_JS/03-control-flujo/06-for.js
@@ -0,0 +1,11 @@
+// El for es como el while pero todo va en una sola linea 
+
+/* for(inicialización; comparacion; terminariteración){
+}*/
+
+for(let i = 0; i < 10; i++){
+    if(i % 2 == 0){
+        console.log('Numeros par', i);
+    }
+}
+
