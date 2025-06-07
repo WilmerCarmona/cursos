b4659e963638c80e0d424f66f1c8c8c7c21fc361
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-06 19:21:14 -0500

    update

--- ULTIMATE_JS/03-control-flujo/03-while.js
+++ ULTIMATE_JS/03-control-flujo/03-while.js
@@ -1,9 +1,21 @@
-// Hasta 20 cuales son los numero pares?
 
+// Ciclo normal
 let i = 0;
 
-while(i <= 20){
+while(i <= 10){
     console.log(i);
-    i+=2
+    i++
 }
- console.log('Fin del ciclo');
+ console.log('Fin del ciclo NORMAL');
+
+ // Ciclo con numero pares
+
+let w = 0;
+ while(w <= 20){
+    if(w % 2 == 0){ // Esto verifica si el número actual es par, si el residuo de dividirlo entre 2 es 0, entonces es par
+        console.log('Numero par',w);
+    }
+    w++;
+ }
+
+ console.log('Fin del ciclo PAR')
