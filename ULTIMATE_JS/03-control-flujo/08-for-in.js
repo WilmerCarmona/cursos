82c1ddacf011f598550ef8ea57feadaa23bb1e9d
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-09 17:01:16 -0500

    Update

--- ULTIMATE_JS/03-control-flujo/08-for-in.js
+++ ULTIMATE_JS/03-control-flujo/08-for-in.js
@@ -1,10 +1,21 @@
-// Nos sirve para que nosotros podamos repetir las opciones de un objeto 
+/* El bucle for in nos permite recorrer todas las propiedades (claves) de un objeto
+por cada propiedad, podemos acceder a su nombre y a su valor*/
 
-let user = { id: 1,
-            name: 'Wilmer',
-            lastname: 'Carmona',
-            age: 22,
+// Creamos un objeto llamado user con varias propiedades
+let user = {
+    id: 1,
+    name: 'Wilmer',
+    lastname: 'Carmona',
+    age: 22,
 };
+
+// Recorremos todas las propiedades del objeto "user"
+for (let prop in user) {
+    // "prop" será, en cada vuelta, el nombre de una propiedad
+    // user[prop] nos da el valor asociado a esa propiedad
+    console.log(user[prop]); // Muestra 1, 'Wilmer', 'Carmona', 22
+}
+
 
 for(let prop in user){
     console.log(user[prop]);
