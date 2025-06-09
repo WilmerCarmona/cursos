30d166359d9547d77c9a397b4af7113c5af29599
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-09 10:00:47 -0500

    update

---
+++ ULTIMATE_JS/03-control-flujo/07-for-of.js
@@ -0,0 +1,16 @@
+
+let animales = ['Perro','Gato','Gallina','Pantera'];
+
+for( let animal of animales){
+    console.log(animal);
+}
+
+//provemos con while
+
+let i = 0;
+
+while(i < animales.length){
+    console.log(animales[i]);
+    i++;
+}
+
