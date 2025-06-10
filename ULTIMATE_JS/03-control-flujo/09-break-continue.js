5f439434ad8cc8bc4429e2734ccae8a5df4d708d
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-10 10:05:11 -0500

    Update

--- ULTIMATE_JS/03-control-flujo/09-break-continue.js
+++ ULTIMATE_JS/03-control-flujo/09-break-continue.js
@@ -9,6 +9,8 @@ while(i < 8){
     if(i === 3){ // Si "i" es exactamente igual a 3 entonces el "Continue" permitirá saltar esta iteración
         continue;
     }
-
+    if (i === 6){ // si "i" es exactamente igual a 6 entonces tal y como lo dice la palabra "break" el codigo saldrá desde esa iteración
+        break;
+    }
     console.log('Este numero es: ', i);
 }
