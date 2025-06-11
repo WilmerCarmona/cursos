46fed740f80ab51777337a62bdd8e6439a92e3dd
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-11 10:13:21 -0500

    Update

---
+++ ULTIMATE_JS/03-control-flujo/10-switch.js
@@ -0,0 +1,13 @@
+let condicion;
+
+switch(condicion){
+    case 'despedir':
+        console.log('Me tengo que ir, hasta luego');
+        break;
+    case 'saludar':
+        console.log('Hola, como te encuentras?');
+        break;
+    default:
+        console.log('No se encontró, ninguna condición');
+}
+
