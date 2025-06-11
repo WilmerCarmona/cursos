59eecc6e9890db3f3a24797d702dbaeb639ae964
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-11 17:23:15 -0500

    update

--- ULTIMATE_JS/04-ejercicios/01.js
+++ ULTIMATE_JS/04-ejercicios/01.js
@@ -1,7 +1,7 @@
 function cualEsMAyor(a,b){
     if(a > b){
         console.log('El numero: ', a , 'Es mayor que', b);
-        
+
     }else if(b > a){
         console.log('El numero: ', b , 'Es mayor que', a);
 
@@ -13,3 +13,11 @@ function cualEsMAyor(a,b){
 let resultado = cualEsMAyor(17, 17);
 
 console.log(resultado);
+
+// Hagamos eso de una forma más corta con el caso ternario 
+
+ function cualEsSuperior(a , b){
+   return (a > b)? a : b; //Ternario
+ }
+ let resul = cualEsSuperior(17,22);
+ console.log(resul)
