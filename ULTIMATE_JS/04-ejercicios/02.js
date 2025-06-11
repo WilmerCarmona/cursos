f99b86f7409c8efaaa99548496509ca8cb3b8434
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-11 18:34:57 -0500

    Update

--- ULTIMATE_JS/04-ejercicios/02.js
+++ ULTIMATE_JS/04-ejercicios/02.js
@@ -28,3 +28,23 @@ function nombreResolucion(ancho , alto){
 
 let resultado = nombreResolucion( 1919 , 720 );
 console.log(resultado);
+
+
+//De una forma mas corta 
+function nombreResolucionn(ancho , alto){
+    if( ancho > 7680 && alto > 4320){
+        return '8k';
+    }else if(ancho >= 3840 && alto >= 2160 ){
+        return '4K';
+    }else if (ancho >= 2560 && alto >= 1440){
+        return 'WQHD';
+    }else if(ancho >= 1920 && alto >= 1080 ){
+        return 'FHD';
+    }else if(ancho >= 1280 && alto >= 720 ){
+        return 'HD';
+    }else{
+        return false;
+    }
+}
+let resul = nombreResolucionn( 2570 , 1500 );
+console.log(resul);
