004b8fa2e277862d8de24268155b609bb558a6d4
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-12 16:20:29 -0500

    Update

--- ULTIMATE_JS/04-ejercicios/04.js
+++ ULTIMATE_JS/04-ejercicios/04.js
@@ -1,10 +1,16 @@
-//Mostrar numeros impares del 1 al 20
+//Mostrar numeros impares hasta el 20
 
 let i = 0;
 
 while(i<=20){
-    if(i%2 !== 0){ //
+    if(i%2 !== 0){ // Utilizo el operador logico de negación "!"
         console.log('Este númerio es impar',i);
     }
     i++;
+}
+
+//Ahora de una forma más corta 
+
+for( let w = 1; w <= 20; w+=2){
+    console.log('Numero impar',w)
 }
