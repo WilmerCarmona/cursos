91b60edb3e5c5de3a18476902f81ee74a3e479e2
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-12 14:11:40 -0500

    Update

--- ULTIMATE_JS/04-ejercicios/03.js
+++ ULTIMATE_JS/04-ejercicios/03.js
@@ -13,3 +13,15 @@ let respuesta = getByIndi([1,2,3,4,5], 4);
 
 console.log(respuesta);
 
+
+//De una forma más corta
+
+function arrayIndice(ar,ind){
+    if(ind<0 || ind>= ar.length){
+        return 'No existe un número con este indice';
+    }
+    return ar[ind];
+}
+
+let solucion = arrayIndice([1,2,3], 1);
+console.log(solucion);
