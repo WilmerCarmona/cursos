17921c96b499c18cc35ee04e40f6f86a5b5655c2
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-13 15:28:06 -0500

    update

--- ULTIMATE_JS/04-ejercicios/05.js
+++ ULTIMATE_JS/04-ejercicios/05.js
@@ -3,14 +3,18 @@
 let array = [ 23, 49, -35, 9, -3, 17];
 
 function getMaxAndMin(arr){
+ // Asumimos que el primer número es el menor y el mayor
   let max = arr[0];
   let min = arr[0];
+  // Recorremos cada número del arreglo
   for(numero of arr){
-    min = min < numero ? min : numero;
-    max = max > numero ? max : numero; 
+    // Si encontramos uno más pequeño, lo asignamos a min
+    min = min < numero ? min : numero; // condicioón "? si_verdadero" y ": si_falso"
+    max = max > numero ? max : numero;  //Si MAX es mayor que numero([0] = 23), se queda igual. En este caso queda en 49
   }
+  // Devolvemos un array con el menor y el mayor
   return[min,max];
 }
 
 let resultado = getMaxAndMin(array);
-console.log(resultado)
+console.log(resultado) // = [-35, 49]
