b27edd7d45a420278dcb5a3e1260eb0d2f7035fa
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-13 16:35:22 -0500

    Update

--- ULTIMATE_JS/04-ejercicios/06.js
+++ ULTIMATE_JS/04-ejercicios/06.js
@@ -3,7 +3,13 @@
 let array = [2, -3, 5, -9, 8, -15];
 
 function getPositive(arr){
-
+    let cantidad = 0; 
+    for( elemento of arr){
+        if(elemento > 0){
+            cantidad++;
+        }
+    }
+    return cantidad;
 }
 
 let resultado = getPositive(array);
