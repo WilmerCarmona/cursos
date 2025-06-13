ed7c03099b22cd41289acfc83a94bb8a64a1b05f
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-13 16:26:17 -0500

    update

---
+++ ULTIMATE_JS/04-ejercicios/06.js
@@ -0,0 +1,10 @@
+// Crear un algoritmo que devuelva la cantidad de número positivos de un array 
+
+let array = [2, -3, 5, -9, 8, -15];
+
+function getPositive(arr){
+
+}
+
+let resultado = getPositive(array);
+console.log(resultado);
