664cdded77f37a81f59c3328f7a1b324e06b1404
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-14 20:23:26 -0500

    update

--- ULTIMATE_JS/04-ejercicios/07.js
+++ ULTIMATE_JS/04-ejercicios/07.js
@@ -1,8 +1,9 @@
 // Crear un algoritno que devuelva el precio del producto más los impuestos de ese producto 
 
 function getPrecioEimpuesto( precio, impuesto){
-    return precio + precio * impuesto;
+    // Calcula el precio final sumando el impuesto al precio base
+    return precio + precio * impuesto; 
 }
 
-let respuesta = getPrecioEimpuesto( 38.500, 0.10);
+let respuesta = getPrecioEimpuesto( 38.500, 0.10);  // Llamamos a la función con 38.500 y 10%
 console.log(respuesta);
