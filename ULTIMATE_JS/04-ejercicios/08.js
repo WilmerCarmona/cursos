72d99302510c04d443d6b69abc27b61c79ac3e2c
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-16 13:59:44 -0500

    Update

--- ULTIMATE_JS/04-ejercicios/08.js
+++ ULTIMATE_JS/04-ejercicios/08.js
@@ -11,3 +11,13 @@ Ejemplo de array de pares
         [2, {nombre: 'Nicolas', apellido: 'Alvarez'}], //par dos identificado con el numero 2
     ];  
 */
+
+let animales =[
+    [0,{edad: 2, nombre: 'Oreo', peso: 2.3}],
+    [1,{edad: 1, nombre: 'Pelusa', peso:1.4}],
+    [3,{edad: 4, nombre: 'Tiger', peso: 4.2}],
+];
+
+function toParis(arr){
+    
+}
