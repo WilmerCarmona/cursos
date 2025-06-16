e5e3974cedf562e336222ef0fd59579e5612ec18
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-16 13:40:20 -0500

    update

--- ULTIMATE_JS/04-ejercicios/08.js
+++ ULTIMATE_JS/04-ejercicios/08.js
@@ -1,6 +1,13 @@
 //crear un algoritmo que tome un array de objetos y devuelva un array de pares 
 
 /*
+Ejemplo de array normal
     let frutas = ["Naranja","manzana","Pomelo"]; // Un array normal solo es una lista de objetos
     console.log( frutas [1]); //El array normal de llamaría de esta forma
+
+Ejemplo de array de pares
+    let personas = [
+        [1, {nombre: 'Wilmer', apellido: 'Carmona'}], //par uno
+        [2, {nombre: 'Nicolas', apellido: 'Alvarez'}], //par dos
+    ];  
 */
