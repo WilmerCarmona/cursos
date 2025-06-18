80d36340d7542c481f9c9c4fb40444cbbea46431
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-18 15:16:31 -0500

    Update

--- ULTIMATE_JS/05-objetos/02-dinanismo.js
+++ ULTIMATE_JS/05-objetos/02-dinanismo.js
@@ -1,1 +1,12 @@
 //Dinanismo: Puedes agregar o quitar datos cuando quieras y puede cambiar su valor en cualquier momento 
+
+
+//Ejemplo
+const user = {id: 1, nombre:'Wilmer', apellido: 'Carmona'};
+
+user.correo = 'wilmercarmona17@gmail.com';
+user.guardar = function(){
+    console.log('Guardando', user.correo);
+}
+
+user.guardar();
