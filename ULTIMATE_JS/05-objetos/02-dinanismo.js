b0dba99caf221a20c8c9b8cdc430975939465743
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-18 15:30:23 -0500

    Update

--- ULTIMATE_JS/05-objetos/02-dinanismo.js
+++ ULTIMATE_JS/05-objetos/02-dinanismo.js
@@ -2,7 +2,8 @@
 
 
 //Ejemplo
-const user = {id: 1, nombre:'Wilmer', apellido: 'Carmona'};
+const user = {id: 1, nombre:'Wilmer', apellido: 'Carmona'}; // const deja agregar a la variable, pero no la deja cambiar
+// user = 1; no dejaria hacer eso.
 
 user.correo = 'wilmercarmona17@gmail.com';
 user.guardar = function(){
