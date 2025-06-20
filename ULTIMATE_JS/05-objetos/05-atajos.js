069dd1beff1c70c945475cfcc9ffc561c68c0f66
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-20 15:22:39 -0500

    Update

--- ULTIMATE_JS/05-objetos/05-atajos.js
+++ ULTIMATE_JS/05-objetos/05-atajos.js
@@ -1,3 +1,22 @@
 let obj = new Object(); // Así es como vemos creando los objetos 
 
 let obj2 = {}; // Éste es un atajo para crear un objetos
+
+// no solo hay atajos de object, tambien hay atajos de:
+/*
+new Array (); = []
+new String (); = "" '' ´´
+new number (); = Numeros 
+new boolean (); = false or true
+*/
+
+function Usuarios(){
+    this.name = "Wilmer";
+    this.lastName = "Carmona";
+    this.recuperarContraseña = function (){
+        console.log('Recuperando...');
+    }
+}
+
+let user = new Usuarios();
+console.log(user.constructor);
