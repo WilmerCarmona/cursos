98d79e7056ed8630bbfcb08c03306553874c5e8e
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-20 16:46:13 -0500

    Update

--- ULTIMATE_JS/05-objetos/06-funciones.js
+++ ULTIMATE_JS/05-objetos/06-funciones.js
@@ -3,6 +3,31 @@ function Usuario(name, age){
     this.edad = age;
 }
 
-let user = new Usuario('Wilmer', 21);
-let user2 = new Usuario('Nicolas', 20);
-console.log(user,user2); 
+console.log(Usuario.name);
+console.log(Usuario.length);
+
+const U = Usuario
+let user = new U ('Wilmer' , 12);
+
+console.log(user);
+
+//Tambien se pueden pasar las funciones como argumento
+//Ejemplo
+
+function of(Fn, arg, arg2){
+    return new Fn(arg,arg2);
+}  
+
+let user2 = of(Usuario , 'Nicolas' ,11); // Este Usuario, viene del constructor que se encuentra al principio
+console.log(user2);
+
+//tambien las podemos retornar dento de otras funciones
+
+function retornar(){
+    return function(){
+        console.log('Hola mundo');
+    }
+}
+
+let saludo = retornar();
+saludo();
