811e3f23c525a81d771c9a284ec4dd9eef70ddcd
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-20 15:33:53 -0500

    update

---
+++ ULTIMATE_JS/05-objetos/06-funciones.js
@@ -0,0 +1,8 @@
+function Usuario(name, age){
+    this.nombre = name;
+    this.edad = age;
+}
+
+let user = new Usuario('Wilmer', 21);
+let user2 = new Usuario('Nicolas', 20);
+console.log(user,user2); 
