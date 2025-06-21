59e1e2313992ced7dd8c34fa37f2e6f903bdf89f
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-21 18:59:52 -0500

    Update

--- ULTIMATE_JS/05-objetos/07-Function.js
+++ ULTIMATE_JS/05-objetos/07-Function.js
@@ -5,9 +5,15 @@ function Punto(x,y){
         console.log('Dibujando...');
     }
 }
+// let imprimir = new Punto(60,65);
+// console.log(imprimir);
 
-let imprimir = new Punto(12,65);
-console.log(imprimir);
+
+//Este es el metodo 'Call' que tienen las funciones 
+let punto = {z : 70}; // Esto lo que nos ayuda es a extender el objeto 
+Punto.call(punto, 60, 65); // luego lo pasamos acá como primer valor, con el valor de los objetos que ya tenemos arriba
+console.log(punto);
+
 
 // Y esta es la forma de 'Function'
 const Point = new Function('x','y',`
