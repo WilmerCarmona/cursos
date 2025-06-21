1e93e4ccee300e754daa87a03c6fbde1583be3d5
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-21 18:45:56 -0500

    update

--- ULTIMATE_JS/05-objetos/07-Function.js
+++ ULTIMATE_JS/05-objetos/07-Function.js
@@ -8,3 +8,15 @@ function Punto(x,y){
 
 let imprimir = new Punto(12,65);
 console.log(imprimir);
+
+// Y esta es la forma de 'Function'
+const Point = new Function('x','y',`
+this.x = x;
+this.y = y;
+this.dibujar = function(){console.log('Dibujando...');}
+`);
+
+const p = new Point(1,2);
+console.log(p);
+
+// Aunque lo mejor es que no se use esta forma
