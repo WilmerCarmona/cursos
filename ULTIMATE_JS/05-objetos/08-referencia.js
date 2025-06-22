481c864ec37f60d443673e623acc8813b840be5f
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-22 13:59:36 -0500

    Update

--- ULTIMATE_JS/05-objetos/08-referencia.js
+++ ULTIMATE_JS/05-objetos/08-referencia.js
@@ -2,5 +2,31 @@ let a = 10;
 let b = a;
 
 b++;
+console.log(a,b);
 
-console.log(b);
+//Ahora con un objeto
+
+let c = {};
+let e = c;
+
+e.prop = 2;
+console.log(c,e);
+
+// Que pasa con las funciones?
+
+let f = 1;
+
+function suma (n){
+    n++
+}
+suma(a);
+console.log(a); // así no sumará el valor ya que la variable f es distinta a la variable f
+
+//
+let g = {prop: 10};
+
+function suma (n){
+    n.prop++
+}
+suma(g);
+console.log(g);
