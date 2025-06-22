6376984fabf0d85037427ca0e3a08982cf5f6042
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-21 19:23:33 -0500

    Update

--- ULTIMATE_JS/05-objetos/07-Function.js
+++ ULTIMATE_JS/05-objetos/07-Function.js
@@ -11,9 +11,15 @@ function Punto(x,y){
 
 //Este es el metodo 'Call' que tienen las funciones 
 let punto = {z : 70}; // Esto lo que nos ayuda es a extender el objeto 
-Punto.call(punto, 60, 65); // luego lo pasamos acá como primer valor, con el valor de los objetos que ya tenemos arriba
-console.log(punto);
+//Punto.call(punto, 60, 65);// luego lo pasamos acá como primer valor, con el valor de los objetos que ya tenemos arriba
+ /*La función 'call' permite llamar funciones a las cuales les puedes pasar el contexto de 'this'
+Y así poder aumentar los objetos que le estas pasando. Con ésto se puede reutilizar codigo*/
 
+/*ademas de 'call' tambien tenemos 'apply' que hace exactamente lo mismo que call, con la diferencia de que todos
+los argumentos que ya tenemos deben de ser pasados con la forma de un array*/
+Punto.apply(punto, [1,2]) ;
+
+console.log(punto);
 
 // Y esta es la forma de 'Function'
 const Point = new Function('x','y',`
