78531ae1f77fdcd092e88a5b2af59176ecd9945e
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-30 12:17:37 -0500

    Update

--- REACT/react-app/src/components/Card.tsx
+++ REACT/react-app/src/components/Card.tsx
@@ -1,9 +1,6 @@
 function Card() {
-  const width = {
-    width: "350px",
-  };
   return (
-    <div className="card" style={width}>
+    <div className="card" style={{ width: "350px" }}>
       <div className="card-body">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">
