26d1547ce2ccccb91aa46ab593fb3b0b32022ee5
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-06-30 17:50:58 -0500

    Update Fragment in Card

--- REACT/react-app/src/components/Card.tsx
+++ REACT/react-app/src/components/Card.tsx
@@ -1,17 +1,33 @@
+import { Fragment } from "react";
+
 function Card() {
   return (
-    <div className="card" style={{ width: "350px" }}>
+    <div
+      className="card"
+      style={{
+        width: "350px",
+      }}
+    >
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card’s content.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
+        {" "}
+        <CardBody></CardBody>{" "}
       </div>
     </div>
+  );
+}
+
+export function CardBody() {
+  return (
+    <Fragment>
+      <h5 className="card-title">Hola mundo</h5>
+      <p className="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card’s content.
+      </p>
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+    </Fragment>
   );
 }
 
