c01145148035e346acd9b76a274192f56b4e2fe9
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-07-02 18:13:37 -0500

    Update List

--- REACT/react-app/src/App.tsx
+++ REACT/react-app/src/App.tsx
@@ -1,9 +1,11 @@
 import Card, { CardBody } from "./components/Card";
+import List from "./components/List";
 
 function App() {
   return (
     <Card>
       <CardBody title="Ésto es un titulo" text="Ésto es un texto" />
+      <List />
     </Card>
   );
 }
