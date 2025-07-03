65814e629e8338513bf2e9884291f45a773df9e4
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-07-03 18:44:07 -0500

    Update elemento in handleClick

--- REACT/react-app/src/components/List.tsx
+++ REACT/react-app/src/components/List.tsx
@@ -3,13 +3,17 @@ import { MouseEvent } from "react";
 type Props = { data: string[] };
 
 function List({ data }: Props) {
-  const handleClick = (e: MouseEvent) => {
+  const handleClick = (e: string) => {
     console.log(e);
   };
   return (
     <ul className="list-group">
       {data.map((elemento) => (
-        <li onClick={handleClick} key={elemento} className="list-group-item">
+        <li
+          onClick={() => handleClick(elemento)}
+          key={elemento}
+          className="list-group-item"
+        >
           {elemento}
         </li>
       ))}
