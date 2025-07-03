b622bfbe29d93b1b1fe6e50e1dd08b79a6f01e9d
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-07-03 17:38:29 -0500

    Update onClick

--- REACT/react-app/src/components/List.tsx
+++ REACT/react-app/src/components/List.tsx
@@ -4,7 +4,11 @@ function List({ data }: Props) {
   return (
     <ul className="list-group">
       {data.map((elemento) => (
-        <li key={elemento} className="list-group-item">
+        <li
+          onClick={() => console.log(elemento)}
+          key={elemento}
+          className="list-group-item"
+        >
           {elemento}
         </li>
       ))}
