33cb3071b9a8f76ac779d7060ba8e8db08c19b13
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-07-04 17:36:49 -0500

    Update List.tsx

--- REACT/react-app/src/components/List.tsx
+++ REACT/react-app/src/components/List.tsx
@@ -10,7 +10,7 @@ function List({ data }: Props) {
         <li
           onClick={() => handleClick(elemento)}
           key={elemento}
-          className="list-group-item"
+          className="list-group-"
         >
           {elemento}
         </li>
