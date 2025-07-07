8f19b7ff415dc6fe5ca6e015b238e4156fe82446
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-07-07 13:06:36 -0500

    color for index

--- REACT/react-app/src/components/List.tsx
+++ REACT/react-app/src/components/List.tsx
@@ -1,16 +1,17 @@
 type Props = { data: string[] };
 
 function List({ data }: Props) {
+  let index = 1;
   const handleClick = (e: string) => {
     console.log(e);
   };
   return (
     <ul className="list-group">
-      {data.map((elemento) => (
+      {data.map((elemento, i) => (
         <li
           onClick={() => handleClick(elemento)}
           key={elemento}
-          className="list-group-item"
+          className={`list-group-item ${index == i ? "active" : ""}`}
         >
           {elemento}
         </li>
