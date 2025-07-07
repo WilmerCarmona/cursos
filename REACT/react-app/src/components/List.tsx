539da1b1ce5786df31e9744ba633ffdbef5307c5
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-07-07 13:13:29 -0500

    changes

--- REACT/react-app/src/components/List.tsx
+++ REACT/react-app/src/components/List.tsx
@@ -2,14 +2,14 @@ type Props = { data: string[] };
 
 function List({ data }: Props) {
   let index = 1;
-  const handleClick = (e: string) => {
-    console.log(e);
+  const handleClick = (i: number) => {
+    index = i;
   };
   return (
     <ul className="list-group">
       {data.map((elemento, i) => (
         <li
-          onClick={() => handleClick(elemento)}
+          onClick={() => handleClick(i)}
           key={elemento}
           className={`list-group-item ${index == i ? "active" : ""}`}
         >
