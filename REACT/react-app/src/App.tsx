f70f8ac832abbcd65b1cfb8e5b5a7645be14f5ec
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-07-07 19:01:21 -0500

    Add handleSelect

--- REACT/react-app/src/App.tsx
+++ REACT/react-app/src/App.tsx
@@ -2,11 +2,14 @@ import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
 
 function App() {
+  const handleSelect = (elemento: string) => {
+    console.log("Imprimiendo", elemento);
+  };
   const list = ["Wilmer", "Nicolas", "Carmona", "Alvarez", "Yesid"];
   return (
     <Card>
       <CardBody title="Ésto es un titulo" text="Ésto es un texto" />
-      <List data={list} />
+      <List data={list} onSelect={handleSelect} />
     </Card>
   );
 }
