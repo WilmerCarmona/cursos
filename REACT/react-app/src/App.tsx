6799fffdad3378f310974a3f1ea3205c87d0c955
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-07-07 19:08:28 -0500

    Example

--- REACT/react-app/src/App.tsx
+++ REACT/react-app/src/App.tsx
@@ -5,11 +5,15 @@ function App() {
   const handleSelect = (elemento: string) => {
     console.log("Imprimiendo", elemento);
   };
+  const handleSelect2 = (elemento: string) => {
+    console.log("Mostrando", elemento);
+  };
   const list = ["Wilmer", "Nicolas", "Carmona", "Alvarez", "Yesid"];
   return (
     <Card>
       <CardBody title="Ésto es un titulo" text="Ésto es un texto" />
       <List data={list} onSelect={handleSelect} />
+      <List data={list} onSelect={handleSelect2} />
     </Card>
   );
 }
