615e9abe8a58ebcd40ce0ddb594fef1b1834d0a0
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-07-09 20:06:09 -0500

    Update Button.tsx

--- REACT/react-app/src/components/Button.tsx
+++ REACT/react-app/src/components/Button.tsx
@@ -3,7 +3,11 @@ import { ReactNode } from "react";
 type Props = { children: ReactNode };
 
 function Button({ children }: Props) {
-  return <div>{children}</div>;
+  return (
+    <button type="button" className="btn btn-primary">
+      {children}
+    </button>
+  );
 }
 
 export default Button;
