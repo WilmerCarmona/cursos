c285747300036975a4f8dbc270acecae5702a8c5
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-07-11 17:40:17 -0500

    Update App.tsx

--- REACT/react-app/src/App.tsx
+++ REACT/react-app/src/App.tsx
@@ -3,26 +3,26 @@ import Button from "./components/Button";
 import List from "./components/List";
 import { useState } from "react";
 
-function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const handleClick = () => setIsLoading(!isLoading);
-  const handleSelect = (elemento: string) => {
-    console.log("Imprimiendo", elemento);
-  };
+// function App() {
+//   const [isLoading, setIsLoading] = useState(false);
+//   const handleClick = () => setIsLoading(!isLoading);
+//   const handleSelect = (elemento: string) => {
+//     console.log("Imprimiendo", elemento);
+//   };
 
-  const list: string[] = ["Wilmer", "Nicolas", "Carmona", "Alvarez"];
-  const contenido = list.length !== 0 && (
-    <List data={list} onSelect={handleSelect} />
-  );
-  return (
-    <Card>
-      <CardBody title="Ésto es un titulo" text="Ésto es un texto" />
-      {contenido}
-      <Button isLoading={isLoading} onClick={handleClick}>
-        Enviar
-      </Button>
-    </Card>
-  );
-}
+//   const list: string[] = ["Wilmer", "Nicolas", "Carmona", "Alvarez"];
+//   const contenido = list.length !== 0 && (
+//     <List data={list} onSelect={handleSelect} />
+//   );
+//   return (
+//     <Card>
+//       <CardBody title="Ésto es un titulo" text="Ésto es un texto" />
+//       {contenido}
+//       <Button isLoading={isLoading} onClick={handleClick}>
+//         Enviar
+//       </Button>
+//     </Card>
+//   );
+// }
 
-export default App;
+// export default App;
