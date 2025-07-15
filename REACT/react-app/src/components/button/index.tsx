c4019896889f51eedced8aeb28338b44dba91fca
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-07-15 15:58:01 -0500

    Changes

--- REACT/react-app/src/components/button/index.tsx
+++ REACT/react-app/src/components/button/index.tsx
@@ -1,17 +1,22 @@
 import { ReactNode } from "react";
 //import styles from "./Button.module.css";
 import styled from "styled-components";
+type BtnProps = {
+  isLoading: boolean;
+};
 type Props = { children: ReactNode; isLoading?: boolean; onClick: () => void };
-const Btn = styled.button`
-  background-color: red;
+const Btn = styled.button<BtnProps>`
+  background-color: ${(props) => (props.isLoading ? "gray" : "red")};
   margin: 10px;
   padding: 25px 30px;
 `;
+
 function Button({ children, isLoading, onClick }: Props) {
   return (
     <Btn
       onClick={onClick}
       disabled={isLoading}
+      isLoading={isLoading}
       // className={[styles.button, styles.padded].join(" ")}
       // className={[
       //   `btn btn-${isLoading ? "secondary" : "primary"}`,
