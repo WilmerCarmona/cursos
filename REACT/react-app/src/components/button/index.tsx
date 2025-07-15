cfdfe9e65d2ab5e8d5532b456e9701c0af6dad7f
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-07-14 20:02:30 -0500

    Add styled

--- REACT/react-app/src/components/button/index.tsx
+++ REACT/react-app/src/components/button/index.tsx
@@ -1,20 +1,17 @@
 import { ReactNode } from "react";
 import styles from "./Button.module.css";
 type Props = { children: ReactNode; isLoading?: boolean; onClick: () => void };
-
 function Button({ children, isLoading, onClick }: Props) {
-  const className = [
-    `btn btn-${isLoading ? "secondary" : "primary"}`,
-    styles.button,
-  ].join(" ");
-
   return (
     <button
       onClick={onClick}
       disabled={isLoading}
       type="button"
       // className={[styles.button, styles.padded].join(" ")}
-      className={className}
+      className={[
+        `btn btn-${isLoading ? "secondary" : "primary"}`,
+        styles.button,
+      ].join(" ")}
     >
       {isLoading ? "Cargando..." : children}
     </button>
